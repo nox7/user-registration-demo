683396b030bafe9010cc7b7aeab0c09be81a44e4
Cole Green <17110935+nox7@users.noreply.github.com>
2023-01-27 13:39:00 -0600

    Setup default controller & service for User

--- src/main/java/org/nox7/userregistrationdemo/user/UserController.java
+++ src/main/java/org/nox7/userregistrationdemo/user/UserController.java
@@ -1,10 +1,26 @@
 package org.nox7.userregistrationdemo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "api/v1/student")
+@RequestMapping(path = "api/v1/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/list")
+    public List<User> listUsers(){
+        return userService.listUsers();
+    }
 
 }
---
+++ src/main/java/org/nox7/userregistrationdemo/user/UserService.java
@@ -0,0 +1,12 @@
+package org.nox7.userregistrationdemo.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    public List<User> listUsers(){
+        return List.of();
+    }
+}
