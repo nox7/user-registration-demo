382276993cbba443bad4d69ba06392f6b4c16c89
Cole Green <17110935+nox7@users.noreply.github.com>
2023-01-27 14:45:45 -0600

    Add DB Utilize repository for service

--- src/main/java/org/nox7/userregistrationdemo/user/UserService.java
+++ src/main/java/org/nox7/userregistrationdemo/user/UserService.java
@@ -1,12 +1,21 @@
 package org.nox7.userregistrationdemo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
     public List<User> listUsers(){
-        return List.of();
+        return userRepository.findAll();
     }
 }
