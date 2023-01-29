e8e7f76f5fb0da5392774303afb6089547d041cc
Cole Green <17110935+nox7@users.noreply.github.com>
2023-01-29 09:01:18 -0600

    Query users by email

--- src/main/java/org/nox7/userregistrationdemo/user/UserRepository.java
+++ src/main/java/org/nox7/userregistrationdemo/user/UserRepository.java
@@ -3,6 +3,10 @@ package org.nox7.userregistrationdemo.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
 }
